48bba53fb6fd004e3ed6f2133b8272fe82f9530d
hd <handytu@gmail.com>
2016-08-17 15:26:05 +0800

    final #2

--- 3-feature_extration_and_preprocessing.py
+++ 3-feature_extration_and_preprocessing.py
@@ -64,7 +64,6 @@ from nltk.stem import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import pos_tag
 
-
 wordnet_tags = ['n','v']
 corpus = [
     'He ate the sandwiches',
@@ -95,5 +94,66 @@ vectorizer = TfidfVectorizer(stop_words='english')
 print(vectorizer.fit_transform(corpus).todense())
 print(vectorizer.vocabulary_)
 
+#哈西技巧实现特征向量：
+from sklearn.feature_extraction.text import HashingVectorizer
+corpus = ['the', 'ate', 'bacon', 'cat']
+vectorizer = HashingVectorizer(n_features=6)
+print(vectorizer.transform(corpus).todense())
 
-        
+#图片特征提取
+#通过像素值提取特征：scikit-learn的digits数字集包括至少1700种0-9的手写数字图像。每个图像都有8x8像像素构成。每个像素的值是0-16，白色是0，黑色是16。
+from sklearn import datasets
+import matplotlib.pyplot as plt
+digits = datasets.load_digits()
+print('Digit:', digits.target[0])
+print(digits.images[0])
+plt.figure()
+plt.axis('off')
+plt.imshow(digits.images[0], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+print('Feature vector:\n', digits.images[0].reshape(-1,64))
+#将8x8矩阵转换成64维向量来创建一个特征向量
+
+
+#兴趣点抽取
+import numpy as np
+from skimage.feature import corner_harris,corner_peaks
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+import  skimage.io as io
+from skimage.exposure import equalize_hist
+
+def show_corners(corners, image):
+    fig = plt.figure()
+    plt.gray()
+    plt.imshow(image)
+    y_corner, x_corner = zip(*corners)
+    plt.plot(x_corner, y_corner, 'or')
+    plt.xlim(0, image.shape[1])
+    plt.ylim(image.shape[0], 0)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * 1.5)
+    plt.show()
+
+mandrill = io.imread('D:\Users\hd\SourceTree\ml_sklearn\mandrill.jpg')
+mandrill = equalize_hist(rgb2gray(mandrill))
+corners = corner_peaks(corner_harris(mandrill), min_distance=2)
+show_corners(corners, mandrill)
+
+#加速稳健特性（SURF）抽取图像兴趣点
+import mahotas as mh
+from mahotas.features import surf
+image = mh.imread('D:\Users\hd\SourceTree\ml_sklearn\mandrill.jpg', as_grey=True)
+print('第一个SURF描述符：\n{}\n'.format(surf.surf(image)[0]))
+print('抽取了%s个SURF描述符' % len(surf.surf(image)))
+
+#使用scale函数进行数据标准化
+from sklearn import preprocessing
+import  numpy as np
+x = np.array([
+    [0., 0., 5., 13., 9., 1.],
+    [0., 0., 13., 15., 10., 15.],
+    [0., 3., 15., 2., 0., 11.]
+])
+print(preprocessing.scale(x))
+
