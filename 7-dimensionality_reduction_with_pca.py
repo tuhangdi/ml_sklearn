5e0356c921b9c310616c5c9a9a339396bc25bdc1
hd <handytu@gmail.com>
2016-08-20 18:46:08 +0800

    no message

---
+++ 7-dimensionality_reduction_with_pca.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2016/8/20 11:26
+@Author  : Thd
+@Site    : 
+@File    : 7-dimensionality_reduction_with_pca.py
+@Software: PyCharm
+"""
